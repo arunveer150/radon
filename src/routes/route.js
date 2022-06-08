6878b12a868b826ea961eca61974d9631da5b56a
Arun Veer <arunveer150@gmail.com>
2022-06-08 15:54:32 +0530

    Class work api post

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,36 +1,53 @@
 const express = require('express');
 const router = express.Router();
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ]
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ]
+       },
+   ]
 
-router.get('/misingNumber-1', function(req,res){
-    let arr= [1,2,3,5,6,7]
-    let sumOfArrayElement = 0
-    let sumOfNumber = 0
-    for(let i=0;i<arr.length;i++){
-        sumOfArrayElement =sumOfArrayElement  + arr[i]
-    }
-      
-    for(let j=0;j<=(arr[0] + arr.length); j++){
-        sumOfNumber=sumOfNumber + j
-    }
+router.post('/createPlayer', function(req,res){
+   let x= req.body
+   let nameRepeat=false
+   for(let i=0;i<players.length;i++){
+       if(players[i].name===x.name){
+           nameRepeat=true
+           break;
+       }
 
-    let missingNumber = sumOfNumber - sumOfArrayElement
-    res.send({data : missingNumber})
-})
-
-router.get('/misingNumber-2', function(req,res){
-    let arr= [33, 34, 35, 37, 38]
-    let sumOfArrayElement = 0
-    let sumOfNumber = 0
-    for(let i=0;i<arr.length;i++){
-        sumOfArrayElement =sumOfArrayElement  + arr[i]
-    }
-      
-    for(let j=arr[0];j<=(arr[0] + arr.length); j++){
-        sumOfNumber=sumOfNumber + j
-    }
-
-    let missingNumber = sumOfNumber - sumOfArrayElement
-    res.send({data : missingNumber})
+       if(nameRepeat){
+          res.send("Name already exists") 
+       } else {
+           players.push(x)
+           
+       }
+   }
+   res.send({data : players})
 })
 
 module.exports = router;
