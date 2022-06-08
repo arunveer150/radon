b7119008af43e2ab10d32d1d2ae845c6f9d5a4b0
Arun Veer <arunveer150@gmail.com>
2022-06-08 15:25:00 +0530

    assignment missing Number

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,109 +1,36 @@
 const express = require('express');
-const lodash= require('lodash')
-
-
-
 const router = express.Router();
 
-router.get('/hello', function (req, res) {
-
-/// Chunk function--------
-const a=["january", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-const chunk=lodash.chunk(a,3)
-console.log(chunk)
-
-
-/// Tail function-------
-const b=[1,3,5,7,9,11,13,15,17,19]
-const tail=lodash.tail(b)
-console.log(tail)
-
-// Union function
-const c=[1,2,3]
-   const d=[1,5,3]
-   const e=[2,6,5,4,]
-   const f=[9,5,6,4,3]
-   const g=[9,4,2,1,5,3,6]
-
-const union=lodash.union(c,d,e,f,g)
-console.log(union)
-
-/// from-pairs function
-const array1=[["TATA", "TIAGO"],["HYUNDAI", "i20"],["MAHINDRA", "THAR"]]
-const pairs=lodash.fromPairs(array1)
-console.log(pairs)
-
-res.get("Done")
-})
-   
-router.get('/movie', function(req,res){
-    const a=["Iron man", "Captain America", "Hulk", "Thor"]
-    console.log(a)
-    res.get("Done")
-})
-
-router.get('/movie/:indexNumber', function(req,res){
-    const a=["Iron man", "Captain America", "Hulk", "Thor"]
-    const b=req.params.indexNumber
-    const check= function(){
-        if(b<a.length){
-    console.log("The movie is : " + a[b])
-        }
-        if(b>=a.length){
-            console.log("Enter Valid Index")
-        } 
+router.get('/misingNumber-1', function(req,res){
+    let arr= [1,2,3,5,6,7]
+    let sumOfArrayElement = 0
+    let sumOfNumber = 0
+    for(let i=0;i<arr.length;i++){
+        sumOfArrayElement =sumOfArrayElement  + arr[i]
     }
-    check()
-    res.get("Done")
+      
+    for(let j=0;j<=(arr[0] + arr.length); j++){
+        sumOfNumber=sumOfNumber + j
+    }
+
+    let missingNumber = sumOfNumber - sumOfArrayElement
+    res.send({data : missingNumber})
 })
 
-router.get('/films',function(req,res){
-    const films=[
-        {"id":1,
-        "name":"Conjuring"
-    },
-    {"id":2,
-    "name":"Life of a pie"
-},
-{"id":3,
-"name":"Transformer"}
-]
- 
-console.log(films)
-res.get("Done")
+router.get('/misingNumber-2', function(req,res){
+    let arr= [33, 34, 35, 37, 38]
+    let sumOfArrayElement = 0
+    let sumOfNumber = 0
+    for(let i=0;i<arr.length;i++){
+        sumOfArrayElement =sumOfArrayElement  + arr[i]
+    }
+      
+    for(let j=arr[0];j<=(arr[0] + arr.length); j++){
+        sumOfNumber=sumOfNumber + j
+    }
+
+    let missingNumber = sumOfNumber - sumOfArrayElement
+    res.send({data : missingNumber})
 })
-
-router.get('/films/:filmId', function(req,res){
-    const films=[
-        {"id":1,
-        "name":"Conjuring"
-    },
-    {"id":2,
-    "name":"Life of a pie"
-},
-{"id":3,
-"name":"Transformer"}
-]
-
-const b=req.params.filmId
-const checkId= function(){
- for(let i=0;i<films.length;i++){
-     if(films[i].id===b){
-         console.log(films[i])
-     }
-     else {continue;}
- }
-//  for(let i=0;i<films.length;i++){
-//      if(films[i].id===b){
-//          continue;
-//      } else {console.log("No such movie exists here")}
-//  }
-}
-checkId()
- res.get("done")
-})
-
-
 
 module.exports = router;
-// adding this comment for no reason
