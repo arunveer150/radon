a14ed79c28c931c54314ad7904ca66202da55079
Arun Veer <arunveer150@gmail.com>
2022-06-11 00:44:39 +0530

    assignment/middleware

--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const mongoose = require('mongoose')
+const Moment = require('moment')
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(
+    function(req,res,next){
+        let d=Moment()
+        let time = d.format("YYYY-MM-DDTHH:mm:ss.ms")
+        let ip = req.socket.remoteAddress
+        let url =  req.originalUrl
+      
+        console.log(time +" ,"+ ip +" ,"+ url )
+        next()
+    }
+)
 
 mongoose.connect("mongodb+srv://arunveer150:Arun-123@cluster0.50bdq.mongodb.net/Populate-Reference-Assignment-4new?retryWrites=true&w=majority", {
     useNewUrlParser: true
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,13 +2,20 @@ const Router  = require('express');
 const express = require('express');
 const router = express.Router();
 const controller = require("../controller/controller.js")
+const Moment = require('moment')
 
 
-router.post('/createAuthor', controller.createAuthor)
- router.post('/createPublisher', controller.createPublisher)
 
-router.post('/createBook', controller.createBook)
-router.get('/getBook', controller.getBook)
-router.put('/changeHardCover', controller.changeHardCover)
-router.put('/updatedPrice', controller.updatedPrice)
+router.get('/API1', function(req,res){
+    res.send("Hello it is first API ")
+})
+router.get('/API2', function(req,res,next){
+    res.send("Hello it is second API ")
+})
+router.get('/API3', function(req,res,next){
+    res.send("Hello it is third API ")
+})
+router.get('/API4', function(req,res,next){
+    res.send("Hello it is fourth API ")
+})
 module.exports = router;
