d5aa45a9f5a3c033e071bddc00e0285eaec88625
Arun Veer <arunveer150@gmail.com>
2022-06-17 18:29:30 +0530

    assignment-axios-weather

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,9 @@
 const Router  = require('express');
 const express = require('express');
 const router = express.Router();
-const userController= require("../controller/userController.js")
-const mid= require("../middleware/auth.js")
+const userController = require("../controller/userController")
 
+router.get('/weather', userController.londonWeather)
+router.get('/sorted', userController.temperature)
 
-
-router.post('/createUser',userController.user)
-router.post('/login', userController.login)
-router.get('/users/:userId', mid.authMiddleware, userController.getUserDetails)
-router.put('/update/:userId',mid.authMiddleware, userController.updateDetails)
-router.delete('/deleted/:userId',mid.authMiddleware, userController.isDeleted)
 module.exports = router;
